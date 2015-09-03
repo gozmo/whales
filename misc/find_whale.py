07f0cd2f5aad1433db391c00269af59c0efdc316
goz <gozmo1337@gmail.com>
2015-09-03 21:55:04 +0200

    add find whale skeleton and crop skeleton

---
+++ misc/find_whale.py
@@ -0,0 +1,21 @@
+from sklearn.cluster import KMeans
+from PIL import Image
+
+im = Image.open("w_7489.jpg")
+
+pixels = []
+coordinates = []
+
+width, height = im.size
+for w in xrange(width):
+    for h in xrange(height):
+        pixels.append(im.getpixel((w,h)))
+        coordinates.append((w,h))
+
+kmeans = KMeans(init='random',
+                n_clusters=3,
+                n_init=10)
+print "clustering"
+a = kmeans.fit(pixels)
+
+print a
