dcce41e0612e5bced7d5b234b4dd96480487cfcc
goz <gozmo1337@gmail.com>
2015-09-05 12:59:10 +0200

    added segmentation function

--- misc/find_whale.py
+++ misc/find_whale.py
@@ -1,20 +1,40 @@
 from sklearn.cluster import KMeans
 from PIL import Image
 
-im = Image.open("w_7489.jpg")
+class SegmentColours:
+    def __init__(self, image_file_name):
+        self._image = Image.open(image_file_name)
+        self._iterations = 10
+        self._clusters = 8
 
-pixels = []
-coordinates = []
+    def segment(self):
+        for x in xrange(self._iterations):
+            print "Iteration: %s, clustering" % x
+            self._cluster_pixels()
+            print "Updating pixels"
+            self._update_pixels()
+        return self._image
 
-width, height = im.size
-for w in xrange(width):
-    for h in xrange(height):
-        pixels.append(im.getpixel((w,h)))
-        coordinates.append((w,h))
 
-kmeans = KMeans(init='random',
-                n_clusters=3,
-                n_init=10)
-print "clustering"
-a = kmeans.fit(pixels)
-print kmeans.cluster_centers_
+    def _cluster_pixels(self):
+        pixels = list(self._image.getdata())
+
+        self._kmeans = KMeans(init='random',
+                        n_clusters=self._clusters,
+                        n_init=10)
+        self._kmeans.fit(pixels)
+
+    def _classify_pixel(self, pixel):
+        return self._kmeans.fit(pixel)
+
+    def _update_pixels(self):
+        width, height = self._image.size
+        for w in xrange(width):
+            for h in xrange(height):
+                coordinate = (w,h)
+                pixel = self._image.getpixel(coordinate)
+                updated_pixel = self._classify_pixel(pixel)
+                self._image.putpixel(coordinate, updated_pixel)
+
+segment_colours = SegmentColours("w_7489.jpg")
+segment_colours.segment()
